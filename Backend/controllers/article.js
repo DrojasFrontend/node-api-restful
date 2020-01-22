5101e50a00ff01de0ce05e9e7aea7eb00acae8ea
DrojasFrontend <danielrojas243@gmail.com>
2020-01-22 13:06:09 -0500

    Cors

--- Backend/controllers/article.js
+++ Backend/controllers/article.js
@@ -286,9 +286,34 @@ var controller = {
     },
 
     search: (req, res) => {
-        return res.status(404).send({
-            status: 'success',
-            message: 'Search '
+        // Sacar el string abuscar
+        var searchString = req.params.search
+
+        Article.find({ "$or" : [
+            { "title": { "$regex": searchString, "$options": "i" } },
+            { "content": { "$regex": searchString, "$options": "i" } }
+        ]})
+        .sort([['date', 'descending']])
+        .exec((err, articles) => {
+            
+            if(err) {
+                return res.status(500).send({
+                    status: 'Error',
+                    message: 'Error en la peticion'
+                })
+            }
+
+            if(!articles || articles.length <= 0) {
+                return res.status(500).send({
+                    status: 'Error',
+                    message: 'No hay articulos que conincidan con tu busqueda'
+                })
+            }
+
+            return res.status(200).send({
+                status: 'success',
+                articles
+            })
         })
     }
 }
